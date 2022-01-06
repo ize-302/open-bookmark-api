73b311c0736919d82547c44d2e6d0b4269f2e652
ozorku <adavizeozorku@gmail.com>
2022-01-06 16:59:09 +0100

    add route to get a bookmark

--- src/routes/bookmark.route.ts
+++ src/routes/bookmark.route.ts
@@ -3,11 +3,13 @@ import { Router } from "express";
 import CreateBookmarkController from "../controllers/Bookmark/Create.controller";
 import DeleteBookmarkController from "../controllers/Bookmark/Delete.controller";
 import BookmarksController from "../controllers/Bookmark/Bookmarks.controller";
+import BookmarkController from "../controllers/Bookmark/Bookmark.controller";
 
 const bookmarkRoute = Router();
 
 bookmarkRoute.post("/bookmarks/create", CreateBookmarkController);
 bookmarkRoute.delete("/bookmarks/:id", DeleteBookmarkController);
 bookmarkRoute.get("/bookmarks", BookmarksController);
+bookmarkRoute.get("/bookmarks/:id", BookmarkController);
 
 module.exports = bookmarkRoute;
