6c5259aff1ed1f67ac62f98654df2df7c5579c7b
ozorku <adavizeozorku@gmail.com>
2022-01-14 15:01:13 +0100

    update CORS

--- src/app.ts
+++ src/app.ts
@@ -10,11 +10,22 @@ dotenv.config();
 app.use(express.json());
 
 // Setup CORS
-let corsOptions = {
-  origin: `http://localhost:8080`,
-  optionsSuccessStatus: 200, // For legacy browser support
-};
-app.use(cors(corsOptions));
+const ALLOWED_ORIGINS = [
+  process.env.FRONTEND_URL,
+  `https://localhost:${process.env.PORT}`,
+];
+app.use((req, res, next) => {
+  let origin = req.headers.origin;
+  let theOrigin =
+    ALLOWED_ORIGINS.indexOf(origin) >= 0 ? origin : ALLOWED_ORIGINS[0];
+  res.header("Access-Control-Allow-Origin", theOrigin);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+
+  next();
+});
 
 // api route
 app.use("/api/v1", api);
